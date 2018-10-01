13b975031857903f2f434949223161b367632c2a
xphong <xphong@gmail.com>
2018-10-01 12:26:09 -0400

    Update config file to single quotes

--- workbox-config.js
+++ workbox-config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{html,css,js}",
-    "img/*.{jpeg,ico,jpg,png,gif}"
+  'globDirectory': 'public/',
+  'globPatterns': [
+    '**/*.{html,css,js}',
+    'img/*.{jpeg,ico,jpg,png,gif}'
   ],
   'globIgnores': [
     'categories/**/*',
     'tags/**/*'
   ],
-  "swDest": "public/sw.js",
-  "swSrc": "src-sw.js"
+  'swDest': 'public/sw.js',
+  'swSrc': 'src-sw.js'
 };
