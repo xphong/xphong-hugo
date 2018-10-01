b6c2150d5cbf8ac292716592c17cbb67894bfb38
xphong <xphong@gmail.com>
2018-10-01 11:54:21 -0400

    Add css and js caching fallbacks

--- src-sw.js
+++ src-sw.js
@@ -1,5 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
+workbox.precaching.precacheAndRoute([]);
+
 workbox.routing.registerRoute(
   /.*\.(?:png|jpg|jpeg|svg|gif)/,
   workbox.strategies.cacheFirst({
@@ -13,4 +15,9 @@ workbox.routing.registerRoute(
   })
 );
 
-workbox.precaching.precacheAndRoute([]);
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'static-resources',
+  }),
+);
