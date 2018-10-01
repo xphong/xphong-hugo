cbd8c013c8cbe1b8a9379db995f89ea2fe0fe682
xphong <xphong@gmail.com>
2018-10-01 11:47:27 -0400

    Remove unused files, update config

--- workbox-config.js
+++ workbox-config.js
@@ -4,6 +4,10 @@ module.exports = {
     "**/*.{html,css,js}",
     "img/*.{jpeg,ico,jpg,png,gif}"
   ],
+  'globIgnores': [
+    'categories/**/*',
+    'tags/**/*'
+  ],
   "swDest": "public/sw.js",
   "swSrc": "src-sw.js"
 };
