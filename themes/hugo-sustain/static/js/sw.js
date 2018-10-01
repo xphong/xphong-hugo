61bda1b0a1902b7e33efe443dbdc78cfe54a23ba
xphong <xphong@gmail.com>
2018-10-01 10:23:41 -0400

    Update naming

---
+++ themes/hugo-sustain/static/js/sw.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+  if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  }
+  console.log('hello service worker');
+
+})();
