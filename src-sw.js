f00c09f6ffd64918e13ba8f61c734f852228ca73
xphong <xphong@gmail.com>
2018-10-01 11:33:10 -0400

    Add separate image cache

--- src-sw.js
+++ src-sw.js
@@ -1,4 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
+workbox.routing.registerRoute(
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
 
 workbox.precaching.precacheAndRoute([]);
